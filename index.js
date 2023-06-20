9782cbb538982d913b0f6b8c85d8533cd142e9f9
Thabo <shaunthabo38@gmail.com>
2023-06-20 21:06:46 +0200

    h commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const myButton = document.querySelector(".btn");
+
+const username = document.querySelector(".username");
+myButton.addEventListener('click', function() {
+
+
+alert('Hello, ' + username.value);
+
+});
